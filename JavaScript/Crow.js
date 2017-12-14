1b0ff6a8d5197d41cf78392c4c61577f39bd9903
Roberto <rmacuna@uninorte.edu.co>
2017-12-13 21:12:23 -0500

    Agregado form validation para los inputs, por ahora solo fue probado con los inputs material, corregido errores relacionados con los input como cuando no detecta el data error y el data positive, pero ahora ya funcionan correctamente

--- JavaScript/Crow.js
+++ JavaScript/Crow.js
@@ -1,26 +1,60 @@
-
 /*---------Inputs---------*/
-jQuery(function($){
+jQuery(function($) {
 
-	$('.m-input').focus(function(){
-		$(this).parent().addClass('is-focused has-label');
-	})
+    $('.m-input').focus(function() {
+        $(this).parent().addClass('is-focused has-label');
+    })
 
-	$('.validate.numbers').on('input', function (e) {
-		var value = $('.validate.numbers').val();
-		if (!$.isNumeric(value)) {
-			console.log('not')
-		}
-		if (value.length == 0) {
-			console.log('Empty')
-		}
-	})
+    $('.validate.numbers').blur(function() {
+        let value = $(this).val();
+        let formId = $(this).parent().attr('id');
+        let className = $(this).attr('class');
+        let elem = $("#" + formId + ">" + "span");
+        //Valido que no se agrege ninguna de las clases de validación como email o letras nada mas
+        if (className.search('letters') == -1 && className.search('email') == -1)  {
+            if (value.length > 0) {
+                if (!$.isNumeric(value)) {
+                	elem.addClass('error');
+                    elem.addClass('show');
+                } else {
+                	elem.addClass('done');
+                	if (elem.attr('class').search('show') == -1) {
+                		elem.addClass('show');
+                	}
+                }
+            } else {
+                elem.removeClass('show');
+            }
+        }
+    })
 
-	$('.m-input').blur(function(){
-		$parent = $(this).parent();
-		if ($(this).val() == '') {
-			$parent.removeClass('has-label');
-		}
-		$parent.removeClass('is-focused');
-	})
+    $('.validate.letters').blur(function() {
+        let value = $(this).val();
+        let formId = $(this).parent().attr('id');
+        let className = $(this).parent().attr('class');
+        let elem =  $("#" + formId + ">" + "span");
+        if (className.search('numbers') == -1 && className.search('email') == -1) {
+        	if (value.length > 0) {
+        		if ($.isNumeric(value)) {
+        			elem.addClass('error');
+        			elem.addClass('show');
+        		}else{
+        			elem.addClass('done');
+        			if (elem.attr('class').search('show') == -1) {
+        				elem.addClass('show');
+        			}
+        		}
+        	}else{
+        		elem.removeClass('show');
+        	}
+        }
+    })
+
+    $('.m-input').blur(function() {
+        $parent = $(this).parent();
+        if ($(this).val() == '') {
+            $parent.removeClass('has-label');
+        }
+        $parent.removeClass('is-focused');
+    })
 })
